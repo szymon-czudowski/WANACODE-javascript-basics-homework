ba9d4eb141eff192182a967dab53dd0248c0d09a
Szymon Czudowski <szymon.czudowski@gmail.com>
2023-08-28 16:52:19 +0200

    feat: New function added to script.js file

--- scripts.js
+++ scripts.js
@@ -1,1 +1,6 @@
-console.log('Hello World!');
+function getGreeting(name, surname) {
+    console.log(`Hi ${name} ${surname}! Good to see you`)
+}
+const greeting = getGreeting('John', 'Smith');
+
+console.log(greeting);
