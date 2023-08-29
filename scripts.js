e440c5f01d68073625b7d24d51e60306d3c8e5b7
Szymon Czudowski <szymon.czudowski@gmail.com>
2023-08-29 22:51:44 +0200

    fix:Simplified isThisMyNameFunction

--- scripts.js
+++ scripts.js
@@ -1,16 +1,12 @@
+// First homework task
 function getGreeting(name, surname) {
     console.log(`Hi ${name} ${surname}! Good to see you`)
 }
 const greeting = getGreeting('John', 'Smith');
-
 console.log(greeting);
-
+// Second homework task
 function isThisMyName(name) {
-    if (name === 'Szymon') {
-        return true;
-    } else {
-        return false;
-    }
+    return !!(name.includes('Szymon'))
 }
-console.log(isThisMyName('Maciek'));
+console.log(isThisMyName('Maciej'));
 console.log(isThisMyName('Szymon'));
