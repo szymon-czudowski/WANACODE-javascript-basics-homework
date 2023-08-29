056ffc80ef5f6e3eb54fd268281ba3ab089b1b84
Szymon Czudowski <szymon.czudowski@gmail.com>
2023-08-30 00:28:19 +0200

    feat:Tasks 3, 4 and 5 added

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 // First homework task
 function getGreeting(name, surname) {
-    console.log(`Hi ${name} ${surname}! Good to see you`)
+    return (`Hi ${name} ${surname}! Good to see you`)
 }
 const greeting = getGreeting('John', 'Smith');
 console.log(greeting);
@@ -10,3 +10,21 @@ function isThisMyName(name) {
 }
 console.log(isThisMyName('Maciej'));
 console.log(isThisMyName('Szymon'));
+
+// Third homework task
+function isThisBoolean(value) {
+    return (value === false || value === true);
+}
+
+// Fourth homework task
+function getCircleArea(radius) {
+    return Math.round(2 * Math.PI * radius);
+}
+const radius = 5;
+const area = getCircleArea(radius);
+console.log(area);
+// Fifth homework task
+function getCirclesAreaSum(radius1, radius2) {
+    return (getCircleArea(radius1) + getCircleArea(radius2));
+}
+console.log(getCirclesAreaSum(5, 8));
