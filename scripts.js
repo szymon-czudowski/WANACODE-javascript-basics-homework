af138469dc08cef0b818c97a44821591613631ac
Szymon Czudowski <szymon.czudowski@gmail.com>
2023-09-06 14:45:23 +0200

    fix: the fixes for all the tasks

--- scripts.js
+++ scripts.js
@@ -1,12 +1,12 @@
 // First homework task
 function getGreeting(name, surname) {
-    return (`Hi ${name} ${surname}! Good to see you`)
+    return `Hi "${name} ${surname}"! What's up?`;
 }
 const greeting = getGreeting('John', 'Smith');
 console.log(greeting);
 // Second homework task
 function isThisMyName(name) {
-    return !!(name.includes('Szymon'))
+    return name.includes('Szymon');
 }
 console.log(isThisMyName('Maciej'));
 console.log(isThisMyName('Szymon'));
@@ -25,21 +25,31 @@ const area = getCircleArea(radius);
 console.log(area);
 // Fifth homework task
 function getCirclesAreaSum(radius1, radius2) {
-    return (getCircleArea(radius1) + getCircleArea(radius2));
+    return getCircleArea(radius1) + getCircleArea(radius2);
 }
 console.log(getCirclesAreaSum(5, 8));
 
 // Sixth homework task
-function rockPaperScissors(player1, player2) {
-    if (player1 === player2 ) {
+function rockPaperScissors(player1Movement, player2Movement) {
+    if (player1Movement === player2Movement ) {
         return 0;
     } else if (
-        player1 === 'scissors' && player2 === 'paper' ||
-        player1 === 'paper' && player2 === 'rock' ||
-        player1 === 'rock' && player2 === 'scissors'
+        player1Movement === 'scissors' && player2Movement === 'paper' ||
+        player1Movement === 'paper' && player2Movement === 'rock' ||
+        player1Movement === 'rock' && player2Movement === 'scissors'
     ) {
         return 1;
     } else {
         return 2;
     }
 }
+function highAndLow(numbers){
+    let arrayOfNumbers = numbers.split(' ').map(numStr => parseInt(numStr, 10))
+    let minNumber = Math.min(...arrayOfNumbers);
+    let maxNumber = Math.max(...arrayOfNumbers);
+    let minNumberOutput = minNumber.toString(10);
+    let maxNumberOutput = maxNumber.toString(10);
+    return `${minNumberOutput} ${maxNumberOutput}`;
+}
+
+console.log(highAndLow(numbers))
